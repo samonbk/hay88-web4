430299153d27fca6d071e162dcb35fcc9b77079f
samonbk <136555363+samonbk@users.noreply.github.com>
2023-07-02 16:58:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var bars = document.getElementById('bars');
+
+
+function myFunction() {
+    if (manu2.style.display === "none") {
+      manu2.style.display = "block";
+    } else {
+      manu2.style.display = "none";
+    }
+  }
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
